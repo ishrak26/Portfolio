da03d1c0d3db49bcac32bbda217fc2359d1cea6b
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2024-08-30 03:36:53 +0600

    Customize some basic sections

--- Constants/projects.js
+++ Constants/projects.js
@@ -15,27 +15,33 @@ export const projects = [
     //leave the githublink field blank if it is a private repo or doent exist, alerts will take care of it
     //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
     {
-        name: 'Dunder Mifflin', //project name
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum', //project description
-        githubLink: '', //github repo link
-        projectLink: '', //deployed project link
-        tech: ['Tech1', 'Tech2', 'Tech3'], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
+        name: 'CineConnect', //project name
+        description:
+            'CineConnect is a comprehensive web platform for cinema enthusiasts. It brings together movie information, user discussions, theater showtimes, and a marketplace for movie fan art - geared towards creating a vibrant community for movie lovers.', //project description
+        githubLink:
+            'https://github.com/ishrak26/CineConnect-Ultimate-Movie-Enthusiasts-Hub', //github repo link
+        projectLink: 'https://cineconnect.vercel.app/', //deployed project link
+        tech: ['Nodejs', 'Nextjs', 'PostgreSQL'], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
         photo: [PROJECT1_1, PROJECT1_2, PROJECT1_3], //list of names of images from above imports.
     },
     {
-        name: 'Project 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
-        tech: ['Tech1', 'Tech2', 'Tech3'],
+        name: 'Football Player Database System',
+        description:
+            'This JavaFX application connects a club to a local server using Java Networking, allowing users to register a club account, view, search, and filter a list of club players. The main challenge was implementing server-based networking to enable player transfers between clubs, with an automatically refreshing transfer market. Currently, only one club can log in from a single program at a time, but future enhancements may include support for multiple logins and a budget-oriented transfer system for a more realistic experience.',
+        githubLink:
+            'https://github.com/ishrak26/Football-Player-Database-System',
+        projectLink:
+            'https://github.com/ishrak26/Football-Player-Database-System',
+        tech: ['JavaFX'],
         photo: [PROJECT2_1, PROJECT2_2, PROJECT2_3],
     },
-    {
-        name: 'Project 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
-        tech: ['Tech1', 'Tech2', 'Tech3'],
-        photo: [PROJECT3_1, PROJECT3_2, PROJECT3_3],
-    },
-]
+    // {
+    //     name: 'Project 3',
+    //     description:
+    //         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
+    //     githubLink: '/',
+    //     projectLink: '/',
+    //     tech: ['Tech1', 'Tech2', 'Tech3'],
+    //     photo: [PROJECT3_1, PROJECT3_2, PROJECT3_3],
+    // },
+];
--- Constants/userinfo.js
+++ Constants/userinfo.js
@@ -1,106 +1,175 @@
 //PLEASE FOLLOW THE FORMAT OF THIS FILE.
 
-import { faGithubAlt, faLinkedinIn, faMediumM, faFacebook, faTwitter, faInstagram, faYoutube, faBehance } from "@fortawesome/free-brands-svg-icons"
+import {
+    faGithubAlt,
+    faLinkedinIn,
+    faMediumM,
+    faFacebook,
+    faTwitter,
+    faInstagram,
+    faYoutube,
+    faBehance,
+} from '@fortawesome/free-brands-svg-icons';
 
 export const userinfo = {
-    logoText: "Michael Scott", //This text is visible on your navbar and footer like your logo.
+    logoText: 'Md. Ishrak Ahsan', //This text is visible on your navbar and footer like your logo.
     contact: {
-        email: 'michaelscott@email.com', //It is always a good idea to mention your email on your website. Good platform to communicate.
-        phone: '1234567890', //Phone number is optional, if you dont want it, consider leaving it blank .
-        countrycode: '+91' //It is advisable to add the country code incase you mention your contact number.
+        email: 'ishrak26@gmail.com', //It is always a good idea to mention your email on your website. Good platform to communicate.
+        phone: '1826071969', //Phone number is optional, if you dont want it, consider leaving it blank .
+        countrycode: '+880', //It is advisable to add the country code incase you mention your contact number.
     },
     socials: [
         //For aesthetics, it is advisable for you to mention upto 4 social media links only. Fill in the links.
         //more icons are imported above, use as you like them.
-        { type: 'github', link: '', icon: faGithubAlt },
-        { type: 'linkedin', link: '', icon: faLinkedinIn },
-        { type: 'medium', link: '', icon: faMediumM },
+        {
+            type: 'github',
+            link: 'https://github.com/ishrak26',
+            icon: faGithubAlt,
+        },
+        {
+            type: 'linkedin',
+            link: 'https://www.linkedin.com/in/ishrak26/',
+            icon: faLinkedinIn,
+        },
+        {
+            type: 'facebook',
+            link: 'https://www.facebook.com/ishrak26',
+            icon: faFacebook,
+        },
+        {
+            type: 'x',
+            link: 'https://x.com/ahsan_ishrak',
+            icon: faTwitter,
+        },
+        {
+            type: 'instagram',
+            link: 'https://www.instagram.com/ahsan_ishrak/',
+            icon: faInstagram,
+        },
+        {
+            type: 'youtube',
+            link: 'https://www.youtube.com/@ishrak26',
+            icon: faYoutube,
+        },
+        // { type: 'behance', link: '', icon: faBehance },
+        // { type: 'medium', link: '', icon: faMediumM },
     ],
     greeting: {
         //this text goes on your landing page
-        title: "Hey there, I am Michael.",
-        subtitle: "Michael Gary Scott is a fictional character in NBC's The Office, portrayed by Steve Carell. Michael is the protagonist of the series, serving as the Regional Manager of the Scranton branch of a paper distribution company, Dunder Mifflin Inc. Also, that's what she said!"
+        title: 'Hey there, I am Md. Ishrak Ahsan',
+        subtitle:
+            'I am a final-year Computer Science and Engineering (CSE) student at Bangladesh University of Engineering and Technology (BUET). My interests lie in algorithms, programming, development of software systems and machine learning. Whenever I am in the field of technology, it is because I love solving problems – may it be creating the next best software app or entering an algorithm challengers. Now that I am about to graduate, I am looking forward to discovering new challenges and taking part in the development of interesting projects in the field.',
     },
     capabilities: [
         //this text goes on your home page in table format.
         //For aesthetics, it is advisable for you to mention upto 4 catgeories links only.
         //For aesthetics, it is advisable for you to keep similar number of skills in each category
         {
-            category: "Category", // eg.frontend, backend, devops etc
-            skills: ["Skillset1", "Skillset1", "Skillset1"] //eg. react, html, python etc.
+            category: 'Programming Languages', // eg.frontend, backend, devops etc
+            skills: [
+                'C/C++',
+                'Java',
+                'Python',
+                'R',
+                'HTML/CSS',
+                'JavaScript',
+                'SQL',
+                'Shell',
+                '8086 Assembly',
+            ], //eg. react, html, python etc.
         },
         {
-            category: "Category",
-            skills: ["Skillset2", "Skillset2", "Skillset2"]
+            category: 'Frontend Development',
+            skills: [
+                'React',
+                'Next.js',
+                'Tailwind CSS',
+                'HTML/CSS',
+                'JavaScript',
+            ],
         },
         {
-            category: "Category",
-            skills: ["Skillset3", "Skillset3", "Skillset3"]
+            category: 'Backend Development',
+            skills: ['Node.js', 'Express', 'PostgreSQL', 'OracleDB', 'SQL'],
         },
         {
-            category: "Category",
-            skills: ["Skillset4", "Skillset4", "Skillset4"]
+            category: 'Machine Learning & Data Science',
+            skills: ['TensorFlow', 'Scikit-learn', 'Numpy', 'Pandas'],
+        },
+        {
+            category: 'Tools',
+            skills: ['Git', 'Latex', 'VS Code', 'IntelliJ IDEA', 'NS-3'],
         },
     ],
     about: {
         //this text goes at the bottom of your home page.
-        content: "'Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.' Whatever the situation or conflict might be, it can almost always be traced back to this need. His conference room characters, fun runs, and copious amounts of office parties are all aimed at his need to be praised.",
-        resume: "/" //link your resume here. it can be drive link or any other link
+        // TODO
+        content:
+            "'Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.' Whatever the situation or conflict might be, it can almost always be traced back to this need. His conference room characters, fun runs, and copious amounts of office parties are all aimed at his need to be praised.",
+        resume: '/', //link your resume here. it can be drive link or any other link
     },
     education: {
         visible: true, //set this to false if you want to omit this section
         educationList: [
             {
-                time: 'May - Oct 2020', //timespan
-                title: 'Degree name', //eg. BTech in Compuster Engineering
-                organization: 'School Name', //eg. VJTI, Mumbai
-                description: 'Lorem ipsum dolor sit amet consectetur' //eg. secured a cgpa of 9.25 in all sems appeared in so far
+                time: 'Feb 2020 - ', //timespan
+                title: 'B.Sc. in Computer Science and Engineering (CSE)', //eg. BTech in Compuster Engineering
+                organization:
+                    'Bangladesh University of Engineering and Technology, Dhaka, Bangladesh', //eg. VJTI, Mumbai
+                description: 'CGPA: 3.97 / 4.00', //eg. secured a cgpa of 9.25 in all sems appeared in so far
             },
             {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
+                time: 'July 2017 - July 2019',
+                title: 'Higher Secondary Certificate (HSC)',
+                organization: 'Notre Dame College, Dhaka, Bangladesh',
+                description: 'GPA: 5.00 / 5.00',
             },
             {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
+                time: 'January 2009 - May 2017',
+                title: 'Secondary School Certificate (SSC)',
+                organization:
+                    'St. Joseph Higher Secondary School, Dhaka, Bangladesh',
+                description: 'GPA: 5.00 / 5.00',
             },
         ],
     },
     experience: {
-        visible: true, //set this to false if you want to omit this section
+        visible: false, //set this to false if you want to omit this section
         experienceList: [
             {
                 company: 'Dunder Mifflin', //company name eg.Microsoft
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*', //companylogo
+                companylogo:
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*', //companylogo
                 position: 'Regional Manager', //post you held eg.Senior SDE
                 time: 'March 2020 - May 2020', //timespan
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum' //eg. spearheaded the graphics team in designing SASS product UIs
+                description:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum', //eg. spearheaded the graphics team in designing SASS product UIs
             },
             {
                 company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
+                companylogo:
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
                 position: 'Regional Manager',
                 time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
+                description:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
             },
             {
                 company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
+                companylogo:
+                    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
                 position: 'Regional Manager',
                 time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
+                description:
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
             },
         ],
     },
     blogs: {
         //set this to false if you want to omit this section
-        visible: true
+        visible: false,
     },
-}
+};
 
 export const headings = {
     //you can customise all the headings here.
@@ -112,7 +181,7 @@ export const headings = {
     experience: 'Experiences',
     blogs: 'I write!',
     contact: 'Contact Me',
-}
+};
 
 export const ctaTexts = {
     //you can customise all the cta texts here.
@@ -121,5 +190,5 @@ export const ctaTexts = {
     capabCTA: 'Get in Touch',
     educationCTA: 'About Me',
     resumeCTA: 'Resume',
-    submitBTN: 'Submit'
-}
+    submitBTN: 'Submit',
+};
